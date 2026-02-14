1465323830fd534d595ace83b903e17f00d884ba
t1ger <tigertv112@gmail.com>
2026-02-14 01:15:23 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 			
 			<button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
 				onClick={ async () => {
-					const res = await getUsers();
+					const res = await getUsers(); 
 					console.log(res);
 				}}
 			>
